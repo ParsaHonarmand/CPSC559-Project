16adf8e0d2c5d854710b57f24f2dd1237e3fb13f
parsahonarmand <parsa.honarmand@gmail.com>
2022-01-27 00:37:26 -0700

    added javadocs

--- CPSC559_Project/src/main/java/Client.java
+++ CPSC559_Project/src/main/java/Client.java
@@ -28,6 +28,13 @@ public class Client {
         return keyboard.nextLine();
     }
 
+    /**
+     * Recursively loops through a directory and calls readFile() on
+     * java source code files
+     * @param files a list of files in the src directory
+     * @param sock socket connection object
+     * @throws IOException
+     */
     public void sendSourceCode(File[] files, Socket sock) throws IOException {
         for (File file : files) {
             if (file.isDirectory()) {
@@ -43,6 +50,11 @@ public class Client {
         }
     }
 
+    /**
+     * Takes a java file as input and reads it line by line and returns it as a string
+     * @param file
+     * @return the string of the source code that's been read
+     */
     public String readFile(File file) {
         String data = "";
         try {
@@ -61,13 +73,25 @@ public class Client {
         }
     }
 
-    // from https://www.baeldung.com/java-file-extension
+    /**
+     * from https://www.baeldung.com/java-file-extension
+     * gets the file extension (java, txt, etc) of the inputted file
+     * @param filename the file to extract the extension from
+     * @return an Optional object that if present, contains the extension as a string
+     */
     public Optional<String> getFileExtension(String filename) {
         return Optional.ofNullable(filename)
                 .filter(f -> f.contains("."))
                 .map(f -> f.substring(filename.lastIndexOf(".") + 1));
     }
 
+    /**
+     * Extracts the peers from the socket reader
+     * and stores them in the peersMap field for this class that
+     * keeps track of these peers
+     * @param reader
+     * @throws IOException
+     */
     public void getPeers(BufferedReader reader) throws IOException {
         int numOfPeers = Integer.parseInt(reader.readLine());
         date = new Date();
@@ -87,6 +111,11 @@ public class Client {
         System.out.println(peersMap);
     }
 
+    /**
+     * Creates a report from the peersMap and
+     * the date that this map's data was retrieved
+     * @return the report as a string
+     */
     public String createReport(){
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String report = peersMap.size() + "\n";
@@ -111,6 +140,12 @@ public class Client {
         return report; 
     }
 
+    /**
+     * Starts the socket and handles all the incoming requests from the registry
+     * @param address the address of the registry
+     * @param port the port in which the registry listens on
+     * @throws IOException
+     */
     public void start(String address, int port) throws IOException {
         setServerAddress(address);
         setServerPort(port);
@@ -151,6 +186,11 @@ public class Client {
         System.out.println("Goodbye...");
     }
 
+    /**
+     * Checks for registry address and port number and if available,
+     * starts the client workflow
+     * @param args address and port
+     */
     public static void main(String[] args) {
         if (args.length == 2) {
             try {
