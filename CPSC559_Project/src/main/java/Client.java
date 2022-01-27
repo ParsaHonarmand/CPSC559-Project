caa7ff5cf6e6e422eb7997f038c9ce3dd22fc883
parsahonarmand <parsa.honarmand@gmail.com>
2022-01-26 18:09:47 -0700

    reporting added

--- CPSC559_Project/src/main/java/Client.java
+++ CPSC559_Project/src/main/java/Client.java
@@ -1,4 +1,6 @@
 // source code is multiple files
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.io.*;
 import java.net.Socket;
 import java.util.Optional;
@@ -8,16 +10,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Client {
     Scanner keyboard = new Scanner(System.in);
     private ConcurrentHashMap<String, Peer> peersMap = new ConcurrentHashMap<>();
-    private String serverAddress = "";
-    private int serverPort;
 //    Peer[] peersSent = null;
-
-    public void setServerAddress(String serverAddress) {
-        this.serverAddress = serverAddress;
-    }
-    public void setServerPort(int serverPort) {
-        this.serverPort = serverPort;
-    }
 
     private String selectTeamName() {
         System.out.print("Enter team name: ");
@@ -86,11 +79,8 @@ public class Client {
         System.out.println(peersMap);
     }
 
-    public void start(String address, int port) throws IOException {
-        setServerAddress(address);
-        setServerPort(port);
-
-        Socket sock = new Socket(address, port);
+    public void start() throws IOException {
+        Socket sock = new Socket("localhost", 55921);
 
         while (sock.isConnected()) {
             InputStream input = sock.getInputStream();
@@ -108,6 +98,27 @@ public class Client {
                 getPeers(reader);
             } else if (serverReqMsg.equals("get report")) {
                 System.out.println("Report request");
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Date date = new Date();
+
+                String report = Integer.toString(peersMap.size()) + "\n";
+
+                for(int i = 0; i < peersMap.size(); i++){
+                    report = report + peersMap.get("placeHolder_teamName").getAddress() + ":" + peersMap.get("placeHolder_teamName").getPort() + "\n";
+                }
+
+                report = report + "1" + "\n" + sock.getInetAddress().getHostAddress() + ":" + "55921" + "\n" + formatter.format(date) + "\n" + peersMap.size();
+
+                for(int i = 0; i < peersMap.size(); i++){
+                    report = report + "\n" + peersMap.get("placeHolder_teamName").getAddress() + ":" + peersMap.get("placeHolder_teamName").getPort() + "\n";
+                }
+
+                System.out.println(report);
+                sock.getOutputStream().write(report.getBytes());
+                sock.getOutputStream().flush();
+                System.out.println("Report sent to host.");
+
+
             } else if (serverReqMsg.equals("close")) {
                 sock.close();
             }
@@ -116,22 +127,11 @@ public class Client {
     }
 
     public static void main(String[] args) {
-        if (args.length == 2) {
-            try {
-                String address = args[0];
-                int port = Integer.parseInt(args[1]);
-                Client client = new Client();
-                try {
-                    client.start(address, port);
-                } catch (IOException ioe) {
-                    ioe.printStackTrace();
-                }
-            } catch (NumberFormatException e) {
-                System.err.println("Argument" + args[0] + " must be an integer.");
-                System.exit(1);
-            }
-        } else {
-            System.out.println("Usage: java Client <ip_address> <port>");
+        Client client = new Client();
+        try {
+            client.start();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
         }
     }
 }
