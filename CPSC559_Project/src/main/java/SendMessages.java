62382ef5588c7205af9437edb12ca279b666e306
Prit Patel <prithome@Prits-MacBook-Pro.local>
2022-03-05 01:00:55 -0700

    Added peer sending every 10 secs

--- CPSC559_Project/src/main/java/SendMessages.java
+++ CPSC559_Project/src/main/java/SendMessages.java
@@ -1,9 +1,13 @@
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 public class SendMessages implements Runnable {
     DatagramSocket udpSocket;
@@ -30,11 +34,46 @@ public class SendMessages implements Runnable {
         }
     }
 
+    private void sendPeer() throws java.io.IOException, InterruptedException{ 
+        TimeUnit.SECONDS.sleep(7);
+
+        Random r = new Random();
+        int numOfPeers = peers.size();
+        int random = r.nextInt(numOfPeers);
+        int randomReceiver = r.nextInt(numOfPeers);
+        
+        Peer receivingPeer = new Peer("", "", 0);
+        Peer randomPeer = new Peer("", "", 0);
+
+        int i = 0;
+        for (Entry<String, Peer> entry : peers.entrySet()) {
+            if (i == random) {
+                randomPeer = entry.getValue();
+            }
+            if (i == randomReceiver) {
+                    receivingPeer = entry.getValue();
+                }
+                i++;
+        }
+
+        DatagramSocket udpPeerSend = new DatagramSocket();
+
+        InetAddress addressReceiverUDP = InetAddress.getByName(receivingPeer.getAddress());
+        String str = "peer" + randomPeer.getAddress() + ":" + randomPeer.getPort();
+        byte[] buf = str.getBytes();
+        DatagramPacket packetSend = new DatagramPacket(buf, buf.length, addressReceiverUDP, receivingPeer.getPort());
+
+        udpPeerSend.send(packetSend);
+        System.out.println("Sent peer at " + randomPeer.getAddress() + ":" +randomPeer.getPort() + " to " + addressReceiverUDP.toString() + ":"+ receivingPeer.getPort());
+        udpPeerSend.close();
+    }
+
     @Override
     public void run() {
         while (Client.isRunning) {
             try {
                 sendSnippet();
+                sendPeer();
                 Thread.sleep(3000);
                 timeStamp += 1;
             } catch (Exception e) {
