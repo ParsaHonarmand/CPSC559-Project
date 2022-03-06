02ed08bad0baa79f84f82e6a4887c697de4a54d5
Prit Patel <prithome@Prits-MacBook-Pro.local>
2022-03-05 18:53:58 -0700

    Added removing inactive peer from peersMap

--- CPSC559_Project/src/main/java/Peer.java
+++ CPSC559_Project/src/main/java/Peer.java
@@ -1,19 +1,25 @@
+import java.time.LocalDateTime;
+
 // File: Peer.java
 public class Peer {
     private String teamName;
     private String address;
     private int port;
+    private LocalDateTime lastNoticed;  
 //    Peer[] peersSent = null;
 
-    public Peer(String teamName, String address, int port) {
+    public Peer(String teamName, String address, int port, LocalDateTime lastNoticed) {
         this.teamName = teamName;
         this.address = address;
         this.port = port;
+        this.lastNoticed = lastNoticed;
+
     }
 
     public String getTeamName() { return teamName; }
     public String getAddress() { return address; }
     public int getPort() { return port; }
+    public LocalDateTime getLastNoticed() { return lastNoticed; }
 
     public void setTeamName(String teamName) {
         this.teamName = teamName;
@@ -23,6 +29,9 @@ public class Peer {
     }
     public void setPort(int port) {
         this.port = port;
+    }
+    public void setLastNoticed(LocalDateTime lastNoticed){
+        this.lastNoticed = lastNoticed;
     }
 
 }
